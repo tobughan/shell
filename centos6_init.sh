47bb69ff5479697e190c15ece672d78f13953c83
tobughan <tobughan@gmail.com>
2017-07-04 16:46:19 +0800

    update centos6_init.sh
    修复内核参数

--- centos6_init.sh
+++ centos6_init.sh
@@ -74,6 +74,11 @@ del_useless_service() {
 #内核参数优化
 sysctl_config() {
 	cat > /etc/sysctl.conf << EOFI
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.default.accept_source_route = 0
+kernel.sysrq = 0
+kernel.core_uses_pid = 1
+net.ipv4.tcp_syncookies = 1
 #消息队列
 kernel.msgmnb = 65536
 kernel.msgmax = 65536
